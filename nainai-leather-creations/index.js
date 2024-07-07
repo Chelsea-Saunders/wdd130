3a4e443fb26f65fca7d7bf60ebc275a641c44ffe
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2024-07-06 22:02:55 -0600

    nainai-homepage/header/login updates

--- nainai-leather-creations/index.js
+++ nainai-leather-creations/index.js
@@ -1,3 +1,6 @@
+
+
+//Java for add to cart button on home page
 addToCartButton = document.querySelectorAll(".add-to-cart-button");
 
 document.querySelectorAll('.add-to-cart-button').forEach(function(addToCartButton) {
@@ -8,3 +11,53 @@ document.querySelectorAll('.add-to-cart-button').forEach(function(addToCartButto
         }, 2000);
     });
 });
+
+
+//Java for sign in page on nav tab
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+
+
+
+
+// Java for slide show on custom orders page
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+} 
