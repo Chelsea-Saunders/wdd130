a1c4df7fcf772c47d955289c67ff5a9b4787b3c1
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2024-07-08 15:57:44 -0600

    nainai custom order page update

--- nainai-leather-creations/index.js
+++ nainai-leather-creations/index.js
@@ -14,17 +14,17 @@ document.querySelectorAll('.add-to-cart-button').forEach(function(addToCartButto
 
 
 //Java for sign in page on nav tab
-<script>
+{/* <script>
 // Get the modal
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close it 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-</script>
+</script> */}
 
 
 
