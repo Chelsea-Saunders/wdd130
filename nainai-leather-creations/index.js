da21f4e1d1b453f097ea520f76ea30edaf87873f
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2024-07-09 06:50:39 -0600

    nainaicustom order page update

--- nainai-leather-creations/index.js
+++ nainai-leather-creations/index.js
@@ -13,8 +13,7 @@ document.querySelectorAll('.add-to-cart-button').forEach(function(addToCartButto
 });
 
 
-//Java for sign in page on nav tab
-{/* <script>
+//Java for log in page on nav tab
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -24,40 +23,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-</script> */}
-
-
-
-
-
-// Java for slide show on custom orders page
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-} 
